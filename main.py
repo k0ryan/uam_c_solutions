a2f2b565c60cb758c7c1d9a8e2b41d23a2fd444e
k0ryan <antoniosolanavera@gmail.com>
2017-09-23 17:23:34 +0200

    Updated for full download

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import subprocess
 
 if __name__ == "__main__":
-    sem_num = input("Numero de la semana\n>>> ")
-    cunt = True
-    ej_num = 0
-    while cunt and ej_num < 20:
-        ej_num = ej_num + 1
-        subprocess.call("wget https://uamx.uam.es/assets/courseware/a61188e6fea1ff94be6d0d5eec38090b/asset-v1:UAMx+ProgC02+2017+type@asset+block/"+str(sem_num)+"_"+str(ej_num)+".c", shell=True)
+    for sem_num in range(1, 15):
+        cunt = True
+        ej_num = 0
+        while cunt and ej_num < 20:
+            ej_num = ej_num + 1
+            subprocess.call("wget https://uamx.uam.es/assets/courseware/a61188e6fea1ff94be6d0d5eec38090b/asset-v1:UAMx+ProgC02+2017+type@asset+block/"+str(sem_num)+"_"+str(ej_num)+".c", shell=True)
 
-    cunt = True
-    ej_num = 0
-    while cunt and ej_num < 10:
-        ej_num = ej_num + 1
-        subprocess.call("wget https://uamx.uam.es/assets/courseware/a61188e6fea1ff94be6d0d5eec38090b/asset-v1:UAMx+ProgC02+2017+type@asset+block/"+str(sem_num)+"_p"+str(ej_num)+".c", shell=True)
+        cunt = True
+        ej_num = 0
+        while cunt and ej_num < 10:
+            ej_num = ej_num + 1
+            subprocess.call("wget https://uamx.uam.es/assets/courseware/a61188e6fea1ff94be6d0d5eec38090b/asset-v1:UAMx+ProgC02+2017+type@asset+block/"+str(sem_num)+"_p"+str(ej_num)+".c", shell=True)
